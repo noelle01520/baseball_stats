804be80166c4f447101dfc83839aa1b145483079
Noelle Corbin <noelle01520@yahoo.com>
2016-03-12 04:18:56 -0500

    added for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160309164258) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "players", force: :cascade do |t|
     t.string   "name"
     t.string   "position"
